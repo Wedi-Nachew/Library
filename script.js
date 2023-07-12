2ca4d8c2a7b9aaa9aecd47418dd12d5e79c4c8b8
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-12 15:51:15 +0300

    Get the inputed data and assign it to the bookInfo object

--- script.js
+++ script.js
@@ -3,20 +3,11 @@ const addBook = document.querySelector(".header > button")
 const addBtn = document.querySelector("form button")
 const formContainer = document.querySelector("#form")
 const form = document.querySelector("form")
+const input = document.querySelectorAll("input")
 
 const myLibrary=[]
 const bookInfo ={}
 
-// Pop up if "Add Book" is clicked
-// Disappear if clicked outside of the form or the Add button
-document.addEventListener("click", (event)=>{
-    if((event.target === addBtn) || 
-        (!form.contains(event.target) && event.target !== addBook)){
-        formContainer.className = "hide"
-    }else if(event.target === addBook){
-        formContainer.className= "show"
-    } 
-})
 
 function Book(title, author, pages, read){
     this.title = title
@@ -28,10 +19,35 @@ function Book(title, author, pages, read){
     }
 }
 
+input.forEach(input=>input.addEventListener("input", (event)=>{
+   switch(event.target.name){
+    case "title":
+        bookInfo.title = event.target.value;
+        break;
+    case "author":
+        bookInfo.author = event.target.value;
+        break;
+    case "pages":
+        bookInfo.pages = event.target.value;
+        break;
+    case "read":
+        bookInfo.read = event.target.value;
+   }
+}))
 
 
 
 
+// Pop up if "Add Book" is clicked
+// Disappear if clicked outside of the form or the Add button
+document.addEventListener("click", (event)=>{
+    if((event.target === addBtn) || 
+        (!form.contains(event.target) && event.target !== addBook)){
+        formContainer.className = "hide"
+    }else if(event.target === addBook){
+        formContainer.className= "show"
+    } 
+})
 
 // Set the default class of the formContainer
 document.addEventListener("DOMContentLoaded", ()=>{
