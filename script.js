b3e7881a688093e9a0d20afc8684b8dc02fdb6e0
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-12 15:07:17 +0300

    Add comment

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// Get reference to the html elements
 const addBook = document.querySelector(".header > button")
 const addBtn = document.querySelector("form button")
 const formContainer = document.querySelector("#form")
+// Add the class show to the formContainer
 addBook.addEventListener("click", ()=>{
     formContainer.className= "show"
 })
+// Add the class hide to the formContainer
 addBtn.addEventListener("click", ()=>{
     formContainer.className = "hide"
 })
+// Set the default class of the formContainer
 document.addEventListener("DOMContentLoaded", ()=>{
     formContainer.className = "hide"
 })
