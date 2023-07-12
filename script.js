bbdc942575cad422fb45dbcb9e153d2bd63bda5d
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-12 14:58:32 +0300

    Make the form pop-up & disppear based on events
    Based on the click events on the Add book and Add buttons the form will
    behave pop-up or disappear from the viewport

---
+++ script.js
@@ -0,0 +1,10 @@
+const addBook = document.querySelector(".header > button")
+const addBtn = document.querySelector("form button")
+const formContainer = document.querySelector("#form")
+addBook.addEventListener("click", ()=>{
+    formContainer.className= "show"
+})
+addBtn.addEventListener("click", ()=>{
+    formContainer.className = "hide"
+})
+
