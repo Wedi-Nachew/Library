a2e9b35fd1e72d3580ff4568c478bc085d28a485
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-12 15:23:39 +0300

    Hide the form if clicked outside of form or Add is clicked

--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 const addBook = document.querySelector(".header > button")
 const addBtn = document.querySelector("form button")
 const formContainer = document.querySelector("#form")
-// Add the class show to the formContainer
-addBook.addEventListener("click", ()=>{
-    formContainer.className= "show"
-})
-// Add the class hide to the formContainer
-addBtn.addEventListener("click", ()=>{
-    formContainer.className = "hide"
+const form = document.querySelector("form")
+
+// Pop up if "Add Book" is clicked
+// Disappear if clicked outside of the form or the Add button
+document.addEventListener("click", (event)=>{
+    if((event.target === addBtn) || 
+        (!form.contains(event.target) && event.target !== addBook)){
+        formContainer.className = "hide"
+    }else if(event.target === addBook){
+        formContainer.className= "show"
+    } 
 })
 // Set the default class of the formContainer
 document.addEventListener("DOMContentLoaded", ()=>{
     formContainer.className = "hide"
 })
+
 
