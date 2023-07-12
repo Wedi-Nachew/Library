21a06060a818045a60c60cbc87e0056b58b83aba
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-12 15:31:09 +0300

    Create an object constructor function

--- script.js
+++ script.js
@@ -17,6 +17,22 @@ document.addEventListener("click", (event)=>{
         formContainer.className= "show"
     } 
 })
+
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    }
+}
+
+
+
+
+
+
 // Set the default class of the formContainer
 document.addEventListener("DOMContentLoaded", ()=>{
     formContainer.className = "hide"
